bb4ad20bf8cd702bbb6950bc9b4eec3bf3c5c6bf
PC <heysoff@outlook.com>
2023-01-06 01:38:05 +0200

    Framework hazırlık 1

---
+++ src/test/java/FeatureFiles/_01_Login.feature
@@ -0,0 +1,8 @@
+
+Feature: Login Functionality
+
+  Scenario: Login with valid username and password
+
+  Given Navigate to Campus
+  When Enter usurname and password and click Login button
+  Then User should login succesfuly
---
+++ src/test/java/StepDefinitions/_01_LoginSteps.java
@@ -0,0 +1,19 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class _01_LoginSteps {
+    @Given("Navigate to Campus")
+    public void navigateToCampus() {
+    }
+
+    @When("Enter usurname and password and click Login button")
+    public void enterUsurnameAndPasswordAndClickLoginButton() {
+    }
+
+    @Then("User should login succesfuly")
+    public void userShouldLoginSuccesfuly() {
+    }
+}
